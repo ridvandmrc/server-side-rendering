ce3ca06721e350995de0ed8389c9fa0c7b4d9fc0
Rıdvan Demirci <ridvandemirci@192.168.1.76>
2023-01-18 00:57:47 +0300

    added serverside rendering props, onlt client side rendering remain

--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -1,5 +1,37 @@
-const ContactUs = () => {
-  return <div> Contact Us</div>;
+import Head from "next/head";
+import { contactUs } from "../../data/contact-us.json";
+
+const ContactUs = (props: any) => {
+  return (
+    <>
+      <Head>
+        <title>Contact- us</title>
+      </Head>
+      <div>
+        <div>
+          <span>Address: </span>
+          <span>{props.data.address}</span>
+        </div>
+        <div>
+          <span>Mail: </span>
+          <span>{props.data.mail}</span>
+        </div>
+        <div>
+          <span>Tel: </span>
+          <span>{props.data.no}</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ContactUs;
+
+// Should use this for fetching data for every request
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      data: contactUs,
+    },
+  };
+};
